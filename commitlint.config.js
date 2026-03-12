e334a253b841fd6a7e50d61b4ba475f983dd98cc
nlklfor <nik.slepcov1771@gmail.com>
2026-03-12 21:40:17 +0100

    ci: add husky, prettier, commitlint and vercel pipeline

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",     // new feature
+        "fix",      // bug fix
+        "chore",    // maintenance, deps
+        "style",    // formatting, no logic change
+        "refactor", // code restructure
+        "perf",     // performance
+        "docs",     // documentation
+        "test",     // tests
+        "revert",   // revert a commit
+        "ci",       // CI/CD changes
+        "build",    // build system changes
+      ],
+    ],
+    "subject-case": [2, "always", "lower-case"],
+    "header-max-length": [2, "always", 72],
+  },
+};
